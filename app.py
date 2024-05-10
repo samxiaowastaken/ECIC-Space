282b2318bcdd6d788cd9a1e8198ee2fee785c83e
samxiaowastaken <samxiaowastaken@outlook.com>
2024-05-10 09:46:33 +0800

    Fix app.py style

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 """Main application module for handling web requests."""
 
+import os
+
 from flask import Flask
 
 
 def create_app():
-    """Create and configure an instance of the Flask application."""
+    """
+    Create and configure an instance of the Flask application.
+
+    Returns:
+        web_app: An instance of the Flask application.
+    """
     web_app = Flask(__name__)
     web_app.config.from_mapping(
-        SECRET_KEY='',
+        SECRET_KEY=os.getenv('SECRET_KEY'),  # Use environment variable
         DEBUG=False,  # Disable debug mode in production
         SQLALCHEMY_DATABASE_URI='sqlite:///your_database.db',
     )
@@ -16,4 +23,4 @@ def create_app():
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(debug=True)  # Ensure debug is set to False in production
+    app.run(debug=False)
